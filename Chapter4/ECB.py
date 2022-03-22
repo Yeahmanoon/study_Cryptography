1c998968a06a38f1cd595849a79dc5c1cfbae5b1
Yeahmano <yamano.hiroya.19@shizuoka.ac.jp>
2022-03-22 15:47:56 +0900

    lim追加

--- Chapter4/ECB.py
+++ Chapter4/ECB.py
@@ -5,16 +5,16 @@
 import sys
 
 #暗号化関数(ECB)
-def E_ECB(plaintext, key):
+def E_ECB(plaintext, key, lim):
     ciphertext = ""
     for i in range(0, len(plaintext), 1):
-        for j in range(0, 128, 1):
+        for j in range(0, lim, 1):
             if ord(plaintext[i]) == key[0][j]:
                 ciphertext = ciphertext + chr(key[1][j])
     return ciphertext 
     
 #復号化関数(ECB)
-def D_ECB(ciphertext, key):
+def D_ECB(ciphertext, key, lim):
     plaintext = ""
     for i in range(0, len(ciphertext), 1):
         for j in range(0, 128, 1):
@@ -29,21 +29,22 @@ if __name__ == '__main__':
 
     plaintext = args[1]
     permutation = int (args[2])
+    lim = 2**7
     key = [ [], [] ]
-    for i in range(0, 128, 1):
+    for i in range(0, lim, 1):
         key[0].append(i)
-        if i + permutation < 128 and i + permutation >= 0:
+        if i + permutation < lim and i + permutation >= 0:
             key[1].append(i + permutation)
-        elif i + permutation >= 128:
-            a = (i + permutation) // 128
-            key[1].append(i + permutation - 128 * a)
-        elif i + permutation < 0 and i + permutation > -128:
-            key[1].append(i + permutation + 128)
+        elif i + permutation >= lim:
+            a = (i + permutation) // lim
+            key[1].append(i + permutation - lim * a)
+        elif i + permutation < 0 and i + permutation > -lim:
+            key[1].append(i + permutation + lim)
         else:
-            a = - (i + permutation) // 128 + 1
-            key[1].append(i + permutation + 128 * a)
+            a = - (i + permutation) // lim + 1
+            key[1].append(i + permutation + lim * a)
 
-    ciphertext = E_ECB(plaintext, key)
+    ciphertext = E_ECB(plaintext, key, lim)
     print(ciphertext)
-    plaintext = D_ECB(ciphertext, key)
+    plaintext = D_ECB(ciphertext, key, lim)
     print(plaintext)
